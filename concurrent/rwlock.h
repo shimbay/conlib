369a2cf2507c95c9f7de1387d4050c5bff1b997b
sunyunbo <sunyunbo@megvii.com>
2019-06-12 12:15:15 +0800

    FIX: rwlock init issue

--- concurrent/rwlock.h
+++ concurrent/rwlock.h
@@ -1,45 +1,45 @@
 #pragma once
 
-#include <pthread.h>
 #include <cassert>
+#include <pthread.h>
 
 namespace conlib {
 
-enum LockType {
-    READ,
-    WRITE
-};
+class RWMutex {
+  friend class ReadLock;
+  friend class WriteLock;
 
-typedef pthread_rwlock_t RWMutex;
+public:
+  RWMutex() { pthread_rwlock_init(&m_mutex, nullptr); }
+
+private:
+  pthread_rwlock_t m_mutex{};
+};
 
 class ReadLock {
 public:
-    explicit ReadLock(RWMutex &mutex) {
-        m_mutex = &mutex;
-        pthread_rwlock_rdlock(m_mutex);
-    }
+  explicit ReadLock(RWMutex &mutex) {
+    m_mutex = &mutex;
+    pthread_rwlock_rdlock(&m_mutex->m_mutex);
+  }
 
-    ~ReadLock() {
-        pthread_rwlock_unlock(m_mutex);
-    }
+  ~ReadLock() { pthread_rwlock_unlock(&m_mutex->m_mutex); }
 
 private:
-    RWMutex *m_mutex;
+  RWMutex *m_mutex;
 };
 
 class WriteLock {
 public:
-    explicit WriteLock(RWMutex &mutex) {
-        m_mutex = &mutex;
-        pthread_rwlock_wrlock(m_mutex);
-    }
+  explicit WriteLock(RWMutex &mutex) {
+    m_mutex = &mutex;
+    pthread_rwlock_wrlock(&m_mutex->m_mutex);
+  }
 
-    ~WriteLock() {
-        pthread_rwlock_unlock(m_mutex);
-    }
+  ~WriteLock() { pthread_rwlock_unlock(&m_mutex->m_mutex); }
 
 private:
-    RWMutex *m_mutex;
+  RWMutex *m_mutex;
 };
 
 } // namespace conlib
